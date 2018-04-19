9fcb051a47ac51d3331dffa6c15bfb05238a186d
clin99 <clin99@csl-408-12.csl.illinois.edu>
2018-04-18 22:43:59 -0500

    Update unittest/taskflow.cpp

--- unittest/taskflow.cpp
+++ unittest/taskflow.cpp
@@ -7,22 +7,85 @@
 #include <utility>
 #include <future>
 #include <tuple>
+#include <thread>
+#include <chrono>
 
 TEST_CASE("TaskFlow"){
+
   tf::Taskflow tf(std::thread::hardware_concurrency());
   REQUIRE(tf.num_workers() == std::thread::hardware_concurrency());
   
   const size_t nof_tasks {100};
-  std::atomic<size_t> counter {0};
- 
+  std::atomic<int> counter {0};
+  std::vector<int64_t> keys;
   std::vector<std::tuple<int64_t,std::future<void>>> tasks;
-  for(size_t i=0;i<nof_tasks;i++){
-    tasks.emplace_back( std::move(tf.emplace([&counter]() {counter += 1;})) );
-  }
-  REQUIRE(tf.num_tasks() == nof_tasks);
 
-  tf.wait_for_all();
-  REQUIRE(counter == nof_tasks);
+  SUBCASE("Without precedence"){
+    for(size_t i=0;i<nof_tasks;i++){
+      tasks.emplace_back( std::move(tf.emplace([&counter]() {counter += 1;})) );
+    }
+    REQUIRE(tf.num_tasks() == nof_tasks);
+
+    tf.wait_for_all();
+    REQUIRE(counter == nof_tasks);
+    REQUIRE(tf.num_tasks() == 0);
+  }
+
+  SUBCASE("With precedence"){
+    for(size_t i=0;i<nof_tasks;i++){
+      tasks.emplace_back( std::move(tf.emplace([&counter]() {counter += 1;})) );
+      if(i>0){
+        tf.precede(std::get<0>(tasks[i-1]), std::get<0>(tasks[i]));
+      }
+    }
+    tf.wait_for_all();
+    REQUIRE(counter == nof_tasks);
+    REQUIRE(tf.num_tasks() == 0);
+  }
+
+  SUBCASE("Silent emplace without precedence"){
+    for(size_t i=0;i<nof_tasks;i++){
+      keys.emplace_back(tf.silent_emplace([&counter]() {counter += 1;}));
+    }
+    tf.wait_for_all();
+    REQUIRE(counter == nof_tasks);
+    REQUIRE(tf.num_tasks() == 0);
+  }
+
+  SUBCASE("Silent emplace with precedence"){
+    for(size_t i=0;i<nof_tasks;i++){
+      keys.emplace_back(tf.silent_emplace([&counter]() {counter += 1;}));
+      if(i>0){
+        tf.precede(keys[i-1], keys[i]);
+      }
+    }
+    tf.wait_for_all();
+    REQUIRE(counter == nof_tasks);
+    REQUIRE(tf.num_tasks() == 0);
+  }
+
+  SUBCASE("Dispatch"){
+    using namespace std::chrono_literals;
+    for(size_t i=0;i<nof_tasks;i++){
+      keys.emplace_back(tf.silent_emplace([&counter]() {counter += 1;}));
+    }
+    auto fu = tf.dispatch();
+    fu.get();
+    REQUIRE(counter == nof_tasks);
+    REQUIRE(tf.num_tasks() == 0);
+  }
+
+  SUBCASE("Silent dispatch"){
+    using namespace std::chrono_literals;
+    for(size_t i=0;i<nof_tasks;i++){
+      keys.emplace_back(tf.silent_emplace([&counter]() {counter += 1;}));
+    }
+    tf.silent_dispatch();
+    std::this_thread::sleep_for(1ms);
+    REQUIRE(counter == nof_tasks);
+    REQUIRE(tf.num_tasks() == 0);
+  }
+  
 
 }
 
