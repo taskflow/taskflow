8f7d9732e74b24d1e993bdbc3b4d49abe80b3b56
twhuang <twh760812@gmail.com>
2018-05-03 11:18:06 -0500

    update simple.cpp

--- example/simple.cpp
+++ example/simple.cpp
@@ -7,21 +7,6 @@
 
 int main(){
 
-tf::Taskflow tf(0);  // force the master thread to execute all tasks
-auto A = tf.silent_emplace([] () { }).name("A");
-auto B = tf.silent_emplace([] () { }).name("B");
-auto C = tf.silent_emplace([] () { }).name("C");
-auto D = tf.silent_emplace([] () { }).name("D");
-auto E = tf.silent_emplace([] () { }).name("E");
-
-A.broadcast(B, C, E);
-C.precede(D);
-B.broadcast(D, E);
-
-std::cout << tf.dump_graphviz();
-
- return 0;
-/*
   tf::Taskflow tf(std::thread::hardware_concurrency());
 
   auto [A, B, C, D] = tf.silent_emplace(
@@ -31,13 +16,13 @@ std::cout << tf.dump_graphviz();
     [] () { std::cout << "TaskD\n"; }
   );
 
-  A.name("A").precede(B);  // B runs after A
+  A.precede(B);  // B runs after A
   A.precede(C);  // C runs after A
   B.precede(D);  // D runs after B
   C.precede(D);  // C runs after D
 
   tf.wait_for_all();  // block until all task finish
-*/
+
   return 0;
 }
 
