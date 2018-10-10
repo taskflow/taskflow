2e638546bfba3f24fdfc5bb3019728cad25a41af
twhuang <twh760812@gmail.com>
2018-10-10 14:54:14 -0500

    updated threadpool unittest

--- unittest/threadpool.cpp
+++ unittest/threadpool.cpp
@@ -162,8 +162,6 @@ void test_external_threads(T& threadpool) {
 template <typename T>
 void test_threadpool() {  
 
-  for(int k=0; k<20000000; ++k) {
-
   SUBCASE("Ownership") {
     for(unsigned i=0; i<=4; ++i) {
       T tp(i);
@@ -190,7 +188,6 @@ void test_threadpool() {
       T tp(i);
       test_external_threads(tp);
     }
-  }
   }
 }
 
