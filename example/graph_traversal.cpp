a7c6c00b1b8fcf0af6ef5b4cdd8a3b88082da8d0
clin99 <clin99@illinois.edu>
2018-12-02 10:49:53 -0600

    Add a graph traversal example to test/profile dynamic tasking. Refactor dynamic tasking to improve its performance. Fix the recusive destructor problem in subgraph.

---
+++ example/graph_traversal.cpp
@@ -0,0 +1,123 @@
+// A simple example to capture the following task dependencies.
+//
+// TaskA---->TaskB---->TaskD
+// TaskA---->TaskC---->TaskD
+
+#include <taskflow/taskflow.hpp>  // the only include you need
+#include <random>
+#include <chrono>
+
+struct Node {
+  size_t level {0};
+  bool visited {false};
+
+  std::atomic<size_t> dependents {0};
+  std::vector<Node*> successors;
+
+  void precede(auto& n) {
+    successors.emplace_back(&n);
+    n.dependents ++;
+  }
+};
+
+void sequential_traversal(std::vector<Node*>& src) {
+  auto start = std::chrono::system_clock::now();
+  while(!src.empty()) {
+    auto n = src.back();
+    assert(!n->visited);
+    n->visited = true;
+    src.pop_back();
+    for(auto& s: n->successors) {
+      if(--s->dependents == 0) {
+        s->level = n->level + 1;
+        src.emplace_back(s);
+      }
+    }
+  }
+  auto end = std::chrono::system_clock::now();
+  std::cout << "Seq runtime: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << '\n'; 
+}
+
+void traverse(Node* n, tf::SubflowBuilder& subflow) {
+  assert(!n->visited);
+  n->visited = true;
+  for(size_t i=0; i<n->successors.size(); i++) {
+    if(--(n->successors[i]->dependents) == 0) {
+      subflow.silent_emplace([s=n->successors[i]](auto &subflow){ traverse(s, subflow); });
+    }
+  }
+}
+
+
+
+int main(){
+
+  std::srand(1);
+  constexpr size_t degree {4};
+  constexpr size_t num_nodes {10000000};
+
+  auto nodes = new Node[num_nodes];
+
+  // A lambda to verify all nodes are visited
+  auto validate = [&nodes](){
+    for(size_t i=0; i<num_nodes; i++) {
+      assert(nodes[i].visited);
+      assert(nodes[i].dependents == 0);
+    }
+  };
+
+  // Make sure nodes are in clean state
+  for(size_t i=0; i<num_nodes; i++) {
+    assert(!nodes[i].visited);
+    assert(nodes[i].successors.empty());
+    assert(nodes[i].dependents == 0);
+  }
+
+  // Create a DAG
+  for(size_t i=0; i<num_nodes; i++) {
+    size_t count {0};
+    for(size_t j=i+1; j<num_nodes && count < degree; j++) {
+      if(rand()%2 == 1) {
+        nodes[i].precede(nodes[j]);
+        count ++;
+      }
+    }
+  }
+
+  // Find source nodes
+  std::vector<Node*> src;
+  for(size_t i=0; i<num_nodes; i++) {
+    assert(nodes[i].successors.size() <= 4);
+    if(nodes[i].dependents == 0) { 
+      src.emplace_back(&nodes[i]);
+    }
+  }
+  //std::cout << "# of source = " << src.size() << '\n';
+
+  //sequential_traversal(src);
+  //size_t l {0};
+  //for(size_t i=0; i<num_nodes; i++) {
+  //  auto& n = nodes[i];
+  //  l = n.level > l ? n.level : l;
+  //}
+  //std::cout << "Level = " << l << '\n';
+  //validate();
+  //return 0;
+
+
+  tf::Taskflow tf(4);
+  for(size_t i=0; i<src.size(); i++) {
+    tf.silent_emplace([i=i, &src](auto& subflow){ traverse(src[i], subflow); });
+  }
+  
+  auto start = std::chrono::system_clock::now();
+  tf.wait_for_all();  // block until finished
+  auto end = std::chrono::system_clock::now();
+  validate();
+  std::cout << "Runtime: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << '\n'; 
+
+  delete[] nodes;
+
+  return 0;
+}
+
