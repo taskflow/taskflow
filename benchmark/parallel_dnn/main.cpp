e5cb106c1babdae5c2c2e3aa72caee5bfc50cceb
clin99 <artiferlly@gmail.com>
2019-03-13 12:42:48 -0500

    Add benchmark Parallel DNN

---
+++ benchmark/parallel_dnn/main.cpp
@@ -0,0 +1,106 @@
+#include <thread>
+#include <iomanip>
+#include "dnn.hpp"
+
+// Function: measure_time_taskflow
+std::chrono::milliseconds measure_time_taskflow(
+  unsigned num_epochs,
+  unsigned num_threads
+) {
+  std::puts("Taskflow");
+  //auto dnn {build_dnn(num_epochs)};
+  auto t1 = std::chrono::high_resolution_clock::now();
+  run_taskflow(num_epochs, num_threads);
+  auto t2 = std::chrono::high_resolution_clock::now();
+  return std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+}
+
+//// Function: measure_time_omp
+//std::chrono::milliseconds measure_time_omp(
+//  unsigned num_epochs,
+//  unsigned num_threads
+//) {
+//  auto dnn {build_dnn(num_epochs)};
+//  auto t1 = std::chrono::high_resolution_clock::now();
+//  run_omp(dnn, num_threads);
+//  auto t2 = std::chrono::high_resolution_clock::now();
+//  return std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+//}
+
+// Function: measure_time_tbb
+std::chrono::milliseconds measure_time_tbb(
+  unsigned num_epochs,
+  unsigned num_threads
+) {
+  std::puts("TBB");
+  //auto dnn {build_dnn(num_epochs)};
+  auto t1 = std::chrono::high_resolution_clock::now();
+  //run_tbb(dnn, num_threads);
+  run_tbb(num_epochs, num_threads);
+  auto t2 = std::chrono::high_resolution_clock::now();
+  return std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+}
+
+// Function: main
+int main(int argc, char *argv[]){
+  
+  unsigned num_threads = std::thread::hardware_concurrency();
+
+  if(argc > 1) {
+    num_threads = std::atoi(argv[1]);
+  }
+
+  {
+    std::string path = std::experimental::filesystem::current_path();
+    path = path.substr(0, path.rfind("cpp-taskflow") + 12);
+    path += "/benchmark/parallel_dnn/";
+
+    IMAGES = read_mnist_image(path + "./train-images.data");
+    LABELS = read_mnist_label(path + "./train-labels.data");
+    TEST_IMAGES = read_mnist_image(path + "./t10k-images-idx3-ubyte");
+    TEST_LABELS = read_mnist_label(path + "./t10k-labels-idx1-ubyte");  
+  }
+
+  ////auto mnist = build_dnn(100); 
+  //MNIST_DNN mnist;
+  //init_dnn(mnist);
+
+  ////run_sequential(mnist, 4);
+  //run_sequential2(mnist, 4);
+  //exit(1);
+
+  int rounds {2};
+
+  //std::cout << std::setw(12) << "# epochs"
+  //          //<< std::setw(12) << "OpenMP"
+  //          << std::setw(12) << "TBB"
+  //          << std::setw(12) << "Taskflow"
+  //          << std::setw(12) << "speedup1"
+  //          << std::setw(12) << "speedup2"
+  //          << '\n';
+
+  for(int epoch=10; epoch<=100; epoch+=10) {
+    
+    //double omp_time {0.0};
+    double tbb_time {0.0};
+    double tf_time  {0.0};
+
+    for(int j=0; j<rounds; ++j) {
+      //omp_time += measure_time_omp(epoch, num_threads).count();
+      tbb_time += measure_time_tbb(epoch, num_threads).count();
+      //tf_time  += measure_time_taskflow(epoch, num_threads).count();
+      exit(1);
+    }
+    
+    std::cout << std::setw(12) << epoch 
+              //<< std::setw(12) << omp_time / rounds / 1e3
+              << std::setw(12) << tbb_time / rounds / 1e3 
+              << std::setw(12) << tf_time  / rounds / 1e3 
+              //<< std::setw(12) << omp_time / tf_time
+              << std::setw(12) << tbb_time / tf_time
+              << std::endl;
+  }
+
+  return EXIT_SUCCESS;
+}
+
---
+++ benchmark/parallel_dnn/taskflow.cpp
@@ -0,0 +1,97 @@
+#include "dnn.hpp"
+#include <taskflow/taskflow.hpp>  
+
+class DNNTrainingPattern : public tf::Framework {
+
+  public:
+
+    DNNTrainingPattern() { 
+      init_dnn(_dnn); 
+      _build_task_graph();
+    };
+
+    void validate(Eigen::MatrixXf &mat, Eigen::VectorXi &vec) {
+      _dnn.validate(mat, vec);
+    }
+
+  private:
+
+    MNIST_DNN _dnn;
+
+    void _build_task_graph() {
+      auto f_task = emplace(
+        [&]() { forward_task(_dnn, IMAGES, LABELS); }
+      );
+
+      std::vector<tf::Task> backward_tasks;
+      std::vector<tf::Task> update_tasks;
+
+      for(int j=_dnn.acts.size()-1; j>=0; j--) {
+        // backward propagation
+        auto& b_task = backward_tasks.emplace_back(emplace(
+          [&, i=j] () { backward_task(_dnn, i, IMAGES); }
+        ));
+
+        // update weight 
+        auto& u_task = update_tasks.emplace_back(
+          emplace([&, i=j] () { _dnn.update(i); })
+        );
+
+        if(j + 1u == _dnn.acts.size()) {
+          f_task.precede(b_task);
+        }
+        else {
+          backward_tasks[backward_tasks.size()-2].precede(b_task);
+        }
+        b_task.precede(u_task);
+      }  
+    }
+};
+
+class DNN : public tf::Framework {
+  public:
+
+    DNN(DNNTrainingPattern &dnn_pattern) : _dnn_pattern(dnn_pattern) {
+      std::vector<tf::Task> tasks;
+      for(auto i=0u; i<NUM_ITERATIONS; i++) {
+        tasks.emplace_back(composed_of(_dnn_pattern));
+      }
+      linearize(tasks);
+    }
+
+  private:
+
+    DNNTrainingPattern& _dnn_pattern;
+};
+
+void run_taskflow(unsigned num_epochs, unsigned num_threads) {
+
+  tf::Taskflow tf {num_threads};
+
+  auto dnn_patterns = std::make_unique<DNNTrainingPattern[]>(NUM_DNNS);
+  auto dnns = std::make_unique<DNN*[]>(NUM_DNNS); 
+
+  for(size_t i=0; i<NUM_DNNS; i++) {
+    dnns[i] = new DNN (dnn_patterns[i]);
+  }
+  
+  std::vector<tf::Task> tasks;
+  tf::Framework parallel_dnn;
+  for(size_t i=0; i<NUM_DNNS; i++) {
+    parallel_dnn.composed_of(*(dnns[i]));
+  }
+
+  auto t1 = std::chrono::high_resolution_clock::now();
+  parallel_dnn.emplace([&](){
+    for(size_t i=0; i<NUM_DNNS; i++) {
+      std::cout << "Validate " << i << "th NN: ";
+      dnn_patterns[i].validate(TEST_IMAGES, TEST_LABELS);
+    }
+    shuffle(IMAGES, LABELS);
+    report_runtime(t1);
+  }).gather(tasks);
+
+  tf.run_n(parallel_dnn, 100).get();
+  //std::cout << parallel_dnn.dump() << std::endl;
+}
+
