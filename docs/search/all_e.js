7432b3558252253455669afc02ffd43329d42857
twhuang <twh760812@gmail.com>
2019-05-13 17:39:58 -0500

    updated docs

--- docs/search/all_e.js
+++ docs/search/all_e.js
@@ -6,7 +6,7 @@ var searchData=
   ['reference',['Reference',['../Reference.html',1,'']]],
   ['release_202_2e0_2e0_20_282018_2f08_2f28_29',['Release 2.0.0 (2018/08/28)',['../release-2-0-0.html',1,'Releases']]],
   ['release_202_2e1_2e0_20_282019_2f02_2f15_29',['Release 2.1.0 (2019/02/15)',['../release-2-1-0.html',1,'Releases']]],
-  ['release_202_2e1_2e0_20_282019_2f05_2f13_29',['Release 2.1.0 (2019/05/13)',['../release-2-2-0.html',1,'Releases']]],
+  ['release_202_2e2_2e0_20_282019_2f05_2f13_29',['Release 2.2.0 (2019/05/13)',['../release-2-2-0.html',1,'Releases']]],
   ['releases',['Releases',['../Releases.html',1,'']]],
   ['run',['run',['../classtf_1_1BasicTaskflow.html#afc7f9ac974ee803b2d3b5577a6c8a2bc',1,'tf::BasicTaskflow::run(Framework &amp;framework)'],['../classtf_1_1BasicTaskflow.html#a876ef3bbdbae7333bd2c3bf0f36a865c',1,'tf::BasicTaskflow::run(Framework &amp;framework, C &amp;&amp;callable)']]],
   ['run_5fn',['run_n',['../classtf_1_1BasicTaskflow.html#abe9f7964777c2d65052b2dda7307efb6',1,'tf::BasicTaskflow::run_n(Framework &amp;framework, size_t N)'],['../classtf_1_1BasicTaskflow.html#a006f2d8acf994a9c6e721b1bcc007b17',1,'tf::BasicTaskflow::run_n(Framework &amp;framework, size_t N, C &amp;&amp;callable)']]],
