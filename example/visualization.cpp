5cee5a4de6b7b5adf1ccda54487e52574ce9c38f
tsung-wei-huang <tsung-wei.huang@utah.edu>
2020-01-03 17:15:25 -0700

    updated example

---
+++ example/visualization.cpp
@@ -0,0 +1,44 @@
+// This example demonstrates how to use 'dump' method to inspect 
+// a taskflow graph.
+
+#include <taskflow/taskflow.hpp>
+
+int main(){
+
+  tf::Taskflow tf("Visualization Demo");
+  
+  // ------------------------------------------------------
+  // Static Tasking
+  // ------------------------------------------------------
+  auto [A, B, C, D, E] = tf.emplace(
+    [] () { std::cout << "Task A" << std::endl; },
+    [] () { std::cout << "Task B" << std::endl; },
+    [] () { std::cout << "Task C" << std::endl; },
+    [] () { std::cout << "Task D" << std::endl; },
+    [] () { std::cout << "Task E" << std::endl; }
+  );
+
+  A.precede(B, C, E); 
+  C.precede(D);
+  B.precede(D, E); 
+  
+  std::cout << "[dump without name assignment]\n";
+  tf.dump(std::cout);
+  
+  std::cout << "[dump with name assignment]\n";
+  A.name("A");
+  B.name("B");
+  C.name("C");
+  D.name("D");
+  E.name("E");
+  tf.dump(std::cout);
+  
+  // ------------------------------------------------------
+  // Dynamic Tasking
+  // ------------------------------------------------------
+  
+
+  return 0;
+}
+
+
