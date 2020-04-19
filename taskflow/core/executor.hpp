2f33773a898f4f24c296e0a7b6b17d1567a2e1a6
Tsung-Wei Huang <twh760812@gmail.com>
2020-04-18 21:38:15 -0600

    updated docs

--- taskflow/core/executor.hpp
+++ taskflow/core/executor.hpp
@@ -184,7 +184,7 @@ class Executor {
     @tparam Observer observer type derived from tf::ExecutorObserverInterface
     @tparam ArgsT... argument parameter pack
 
-    @param args... arguments to forward to the constructor of the observer
+    @param args arguments to forward to the constructor of the observer
     
     @return a raw pointer to the observer associated with this executor
     */
