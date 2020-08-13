0299f40f05c701eba0df48f014fa3f4e9e4ab6ff
tsung-wei-huang <tsung-wei.huang@utah.edu>
2020-08-12 19:48:56 -0600

    updated black scholes

---
+++ benchmarks/black_scholes/taskflow.cpp
@@ -0,0 +1,34 @@
+#include "common.hpp"
+#include <taskflow/taskflow.hpp>
+
+
+void bs_taskflow(unsigned num_threads) {
+
+  tf::Executor executor(num_threads);
+  tf::Taskflow taskflow;
+
+  taskflow.parallel_for(
+    0, numOptions, 1, [&](int i) {
+      /* Calling main function to calculate option value based on 
+       * Black & Scholes's equation.
+       */
+       auto price = BlkSchlsEqEuroNoDiv( sptprice[i], strike[i],
+                                    rate[i], volatility[i], otime[i], 
+                                    otype[i], 0);
+       prices[i] = price;
+#ifdef ERR_CHK 
+      check_error(i, price);
+#endif
+    } 
+  );
+
+  executor.run_n(taskflow, NUM_RUNS).wait();
+}
+
+
+std::chrono::microseconds measure_time_taskflow(unsigned num_threads) {
+  auto beg = std::chrono::high_resolution_clock::now();
+  bs_taskflow(num_threads);
+  auto end = std::chrono::high_resolution_clock::now();
+  return std::chrono::duration_cast<std::chrono::microseconds>(end - beg);
+}
