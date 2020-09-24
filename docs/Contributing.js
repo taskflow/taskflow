6e69ab4f79b60b985e8e4bfa51bd7ef4269f9807
tsung-wei-huang <tsung-wei.huang@utah.edu>
2020-09-24 14:45:46 -0600

    updated docs

--- docs/Contributing.js
+++ docs/Contributing.js
@@ -1,6 +1,18 @@
 var Contributing =
 [
-    [ "Guidelines", "guidelines.html", null ],
+    [ "Guidelines", "guidelines.html", [
+      [ "How Can I Contribute?", "guidelines.html#HowCanIContribute", null ],
+      [ "How Can I Get Credited?", "guidelines.html#HowCanIGetCredited", null ],
+      [ "How Can I Get Started?", "guidelines.html#HowCanIGetStarted", [
+        [ "Step 1: Look around", "guidelines.html#Step1LookAround", null ],
+        [ "Step 2: Write a Taskflow program", "guidelines.html#Step2WriteATaskflowProgram", null ],
+        [ "Step 3: Dive in", "guidelines.html#Step3WriteATaskflowProgram", null ]
+      ] ],
+      [ "How Can I Report Issues?", "guidelines.html#HowCanIReportAnIssue", null ],
+      [ "How Can I Edit the Documentation?", "guidelines.html#HowCanIEditTheDocumentation", null ],
+      [ "How Can I Submit a Patch?", "guidelines.html#HowCanISubmitAPatch", null ],
+      [ "How Can I Lead a Project?", "guidelines.html#HowCanILeadAProject", null ]
+    ] ],
     [ "Code of Conduct", "codeofconduct.html", [
       [ "Taskflow Community Code of Conduct", "codeofconduct.html#TaskflowCodeOfConduct", null ],
       [ "Report Violations", "codeofconduct.html#ReportViolations", null ]
--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -1,40 +1,17 @@
-/*
-@ @licstart  The following is the entire license notice for the
-JavaScript code in this file.
-
-Copyright (C) 1997-2017 by Dimitri van Heesch
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-@licend  The above is the entire license notice
-for the JavaScript code in this file
-*/
 var NAVTREE =
 [
   [ "Taskflow", "index.html", [
-    [ "Quick Start", "index.html", [
-      [ "Modern C++ Parallel Task Programming", "index.html#ModernCppParallelTaskProgrammingLibrary", null ],
-      [ "Static and Dynamic Tasking", "index.html#StaticDynamicTasking", null ],
-      [ "Conditional Tasking", "index.html#ConditionalTasking", null ],
-      [ "Composable Tasking", "index.html#ComposableTasking", null ],
-      [ "Concurrent CPU-GPU Tasking", "index.html#HeterogeneousTasking", null ],
-      [ "How to Install Taskflow?", "index.html#HowToInstallCppTaskflow", null ],
-      [ "A Simple First Program", "index.html#ASimpleFirstProgram", null ],
-      [ "Compile and Run Your First Program", "index.html#CompileAndRunYourFirstProgram", null ],
-      [ "Supported Compilers", "index.html#SupportedCompilers", null ],
-      [ "License", "index.html#License", null ]
+    [ "Quick Start", "QuickStart.html", [
+      [ "Modern C++ Parallel Task Programming", "QuickStart.html#ModernCppParallelTaskProgrammingLibrary", null ],
+      [ "Static and Dynamic Tasking", "QuickStart.html#StaticDynamicTasking", null ],
+      [ "Conditional Tasking", "QuickStart.html#ConditionalTasking", null ],
+      [ "Composable Tasking", "QuickStart.html#ComposableTasking", null ],
+      [ "Concurrent CPU-GPU Tasking", "QuickStart.html#HeterogeneousTasking", null ],
+      [ "How to Install Taskflow?", "QuickStart.html#HowToInstallCppTaskflow", null ],
+      [ "A Simple First Program", "QuickStart.html#ASimpleFirstProgram", null ],
+      [ "Compile and Run Your First Program", "QuickStart.html#CompileAndRunYourFirstProgram", null ],
+      [ "Supported Compilers", "QuickStart.html#SupportedCompilers", null ],
+      [ "License", "QuickStart.html#License", null ]
     ] ],
     [ "Building and Installing", "install.html", [
       [ "Setting Up Taskflow", "install.html#BAISettingUpTaskflow", null ],
@@ -88,8 +65,8 @@ var NAVTREE =
         [ "Variables", "functions_vars.html", null ]
       ] ]
     ] ],
-    [ "Files", "files.html", [
-      [ "File List", "files.html", "files_dup" ]
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
     ] ]
   ] ]
 ];
@@ -97,7 +74,7 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 ".html",
-"codeofconduct.html#ReportViolations"
+"classtf_1_1cudaTask.html#a1b0da9e643b80008063406fe1bf207b9"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
