775a6f4287518450396c347640ee6818d85149bd
Tsung-Wei Huang <twh760812@gmail.com>
2020-09-24 15:48:50 -0600

    before merge

--- docs/Contributing.js
+++ docs/Contributing.js
@@ -1,6 +1,8 @@
 var Contributing =
 [
-    [ "Guidelines", "guidelines.html", null ],
+    [ "Guidelines", "guidelines.html", [
+      [ "How Can I Contribute?", "guidelines.html#HowCanIContribute", null ]
+    ] ],
     [ "Code of Conduct", "codeofconduct.html", [
       [ "Taskflow Community Code of Conduct", "codeofconduct.html#TaskflowCodeOfConduct", null ],
       [ "Report Violations", "codeofconduct.html#ReportViolations", null ]
