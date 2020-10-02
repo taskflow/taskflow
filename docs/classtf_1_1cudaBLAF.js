46a18a58a8378aa6538815ee6a58663721fb3681
Tsung-Wei Huang <twhuang@meb-2124-01.ece.utah.edu>
2020-10-02 13:49:11 -0600

    updated docs

---
+++ docs/classtf_1_1cudaBLAF.js
@@ -0,0 +1,5 @@
+var classtf_1_1cudaBLAF =
+[
+    [ "cudaBLAF", "classtf_1_1cudaBLAF.html#a3525fd83f636825d25e8f4740a1e8cb2", null ],
+    [ "transpose", "classtf_1_1cudaBLAF.html#a35fad7f1b42978cb5495d4db19ef0cb9", null ]
+];
