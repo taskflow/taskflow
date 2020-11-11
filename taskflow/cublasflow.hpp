d917736dc3d1965d9fb1f7400e58e56126c74d80
Tsung-Wei Huang <twh760812@gmail.com>
2020-11-10 21:34:32 -0700

    updated docs

--- taskflow/cublasflow.hpp
+++ taskflow/cublasflow.hpp
@@ -15,3 +15,10 @@
 #include "cuda/cublas/cublas_level1.hpp"
 #include "cuda/cublas/cublas_level2.hpp"
 #include "cuda/cublas/cublas_level3.hpp"
+
+/**
+@file cublasflow.hpp
+@brief main cublasFlow include file
+*/
+
+
--- taskflow/cudaflow.hpp
+++ taskflow/cudaflow.hpp
@@ -7,3 +7,8 @@
 
 #include "taskflow.hpp"
 #include "cuda/cuda_flow.hpp"
+
+/**
+@file cudaflow.hpp
+@brief main cudaFlow include file
+*/
