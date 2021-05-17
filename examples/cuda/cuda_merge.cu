82c0d7048819efd949b12d53176765218f3495ef
Tsung-Wei Huang <twh760812@gmail.com>
2021-05-16 19:04:21 -0600

    updated docs

--- examples/cuda/cuda_merge.cu
+++ examples/cuda/cuda_merge.cu
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[]) {
   
   if(argc != 2) {
-    std::cerr << "usage: ./cuda_scan N\n";
+    std::cerr << "usage: ./cuda_merge N\n";
     std::exit(EXIT_FAILURE);
   }
 
