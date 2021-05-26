8c9f3e283da81a8ca8d85a49bb72fbd8b958fa45
z-adams <chryxus@gmail.com>
2021-05-25 17:04:41 -0700

    Removed thread ref member from worker

--- taskflow/core/worker.hpp
+++ taskflow/core/worker.hpp
@@ -27,7 +27,6 @@ class Worker {
     Notifier::Waiter* _waiter;
     std::default_random_engine _rdgen { std::random_device{}() };
     TaskQueue<Node*> _wsq;
-    std::thread* _thread;
 };
 
 // ----------------------------------------------------------------------------
