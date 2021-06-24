d4de8cd62e6b32cccc2fee09009414a3c8bc9e59
twhuang@meb-2124-01 <tsung-wei.huang@utah.edu>
2021-06-24 13:53:31 -0600

    updated nonreachable_sanitizer.hpp

--- taskflow/sanitizer/sanitizer.hpp
+++ taskflow/sanitizer/sanitizer.hpp
@@ -1,22 +1,23 @@
 #pragma once
 
-#include "../core/taskflow.hpp"
+#include "nonreachable_sanitizer.hpp"
 
 namespace tf {
 
 class Sanitizer {
 
-
   public:
-
-    bool check_nonreachable(const Taskflow& taskflow, std::ostream& os) {
+    
+    // TODO: (McKay)
+    // migrate your implementation to nonreachable_sanitizer.hpp
+    // and write some examples to test (in examples/sanitizer.cpp)
+    std::vector<Task> check_nonreachable(const Taskflow& taskflow, std::ostream& os) {
       
       os << "hello I am checking your taskflow with nonreachable tasks\n";
-      
-      // TODO (McKay): design the graph data structure extracted from taskflow
-      //               understand tf::Taskflow::dump
 
-      return false;
+      NonReachableSanitizer san(taskflow);
+
+      return san(os);
     }
 
 };
