5291389f4abc0443e289fc5d1c6260e184768499
twhuang@meb-2124-01 <tsung-wei.huang@utah.edu>
2021-09-06 15:30:59 -0600

    fixed depth for stack overflow

--- unittests/traverse.cpp
+++ unittests/traverse.cpp
@@ -175,7 +175,7 @@ void parallel_traverse(unsigned num_threads) {
 
       std::atomic<size_t> level {0};
 
-      size_t max_degree = 8;
+      size_t max_degree = 4;
       size_t num_nodes = 1000;
       
       auto nodes = make_dag(num_nodes, max_degree);
