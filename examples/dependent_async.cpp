e578ea00efa64402b4d0c1bebd6e5b9eda785c70
twhuang <twhuang@pass-5.ece.utah.edu>
2023-04-26 15:59:58 -0600

    fixed run fail

---
+++ examples/dependent_async.cpp
@@ -0,0 +1,27 @@
+#include <taskflow/taskflow.hpp>  // the only include you need
+
+int main(){
+
+  tf::Executor executor;
+
+  std::vector<tf::AsyncTask> range;
+
+  auto A = executor.silent_dependent_async(
+    "A", [](){ std::cout << "A\n"; }, range.begin(), range.end()
+  );
+  
+  auto B = executor.silent_dependent_async(
+    "B", [](){ std::cout << "B\n"; }, range.begin(), range.end()
+  );
+
+  range.push_back(A);
+  range.push_back(B);
+
+  auto C = executor.silent_dependent_async(
+    "C", [](){ std::cout << "C\n"; }, range.begin(), range.end()
+  );
+
+  executor.wait_for_all();
+
+  return 0;
+}
