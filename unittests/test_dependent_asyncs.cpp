c8c032ad9bffe7104a940d4d1ec33084790e9055
cheng-hsiang-chiu <chchiu@pass-2.ece.utah.edu>
2023-06-08 14:01:19 -0600

    added two more tests

--- unittests/test_dependent_asyncs.cpp
+++ unittests/test_dependent_asyncs.cpp
@@ -649,7 +649,7 @@ void binary_tree(unsigned W) {
   
   // iterate all other tasks level by level
   for(size_t i=0; i<L; i++) {
-    for(size_t n=0; n < (1<<i); n++) {
+    for(size_t n=0; n < static_cast<size_t>(1<<i); n++) {
       if(task_id == 1) {
         tasks_c.push_back(
           executor.silent_dependent_async(
@@ -681,7 +681,7 @@ void binary_tree(unsigned W) {
   
   task_id = 1;
   for(size_t i=0; i<L; i++) {
-    for(size_t n=0; n<(1<<i); n++) {
+    for(size_t n=0; n<static_cast<size_t>(1<<i); n++) {
       REQUIRE(data[task_id] == i + 1);
       //printf("data[%zu]=%d\n", task_id, data[task_id]);
       task_id++;
@@ -889,4 +889,95 @@ TEST_CASE("DependentAsync.ParallelGraphConstruction.16threads" * doctest::timeou
   parallel_graph_construction(16);
 }
 
+// ----------------------------------------------------------------------------
+// Iterative Fibonacci 
+// ----------------------------------------------------------------------------
+std::vector<size_t> fibonacci{0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040,1346269,2178309,3524578,5702887,9227465,14930352,24157817,39088169,63245986,102334155,165580141,267914296,433494437,701408733,1134903170,1836311903,2971215073,4807526976,7778742049,12586269025,20365011074,32951280099,53316291173,86267571272,139583862445,225851433717,365435296162,591286729879,956722026041,1548008755920,2504730781961,4052739537881,6557470319842,10610209857723,17167680177565,27777890035288,44945570212853,72723460248141,117669030460994,190392490709135,308061521170129,498454011879264,806515533049393,1304969544928657,2111485077978050,3416454622906707,5527939700884757,8944394323791464,14472334024676221,23416728348467685,37889062373143906,61305790721611591,99194853094755497,160500643816367088,259695496911122585,420196140727489673,679891637638612258,1100087778366101931,1779979416004714189,2880067194370816120,4660046610375530309,7540113804746346429};
+void iterative_fibonacci(unsigned W) {
+
+  tf::Executor executor(W);
+  
+  std::vector<tf::AsyncTask> tasks;
+  
+  size_t val_n_1 = 0, val_n_2 = 0;
+
+  for (int i = 0; i <= 92; ++i) {
+    if (i < 2) {
+      auto [t, fut] = executor.dependent_async([i](){ return i; });
+      tasks.push_back(t);
+      val_n_2 = val_n_1;
+      val_n_1 = fut.get();
+    }
+    else {
+      auto [t, fut] = executor.dependent_async([i, val_n_1, val_n_2, &tasks](){
+        return val_n_2 + val_n_1;
+      }, tasks[i-1], tasks[i-2]);
+      
+      tasks.push_back(t);
+      val_n_2 = val_n_1;
+      val_n_1 = fut.get();
+    }
+    REQUIRE(val_n_1 == fibonacci[i]);  
+  }
+}
+
+TEST_CASE("DependentAsync.IterativeFibonacci.1thread" * doctest::timeout(300)) {
+  iterative_fibonacci(1);
+}
+
+TEST_CASE("DependentAsync.IterativeFibonacci.2threads" * doctest::timeout(300)) {
+  iterative_fibonacci(2);
+}
+
+TEST_CASE("DependentAsync.IterativeFibonacci.4threads" * doctest::timeout(300)) {
+  iterative_fibonacci(4);
+}
+
+TEST_CASE("DependentAsync.IterativeFibonacci.8threads" * doctest::timeout(300)) {
+  iterative_fibonacci(8);
+}
+
+// ----------------------------------------------------------------------------
+// Recursive Fibonacci 
+// ----------------------------------------------------------------------------
+void recursive_fibonacci(unsigned W) {
+
+  tf::Executor executor(W);
+  
+  std::function<int(int)> fib;
+
+  fib = [&](int N){
+    if (N < 2) {
+      return N; 
+    }
+    auto [t1, fu1] = executor.dependent_async(std::bind(fib, N-1));
+    auto [t2, fu2] = executor.dependent_async(std::bind(fib, N-2));
+
+    executor.corun_until([&](){ return t1.is_done(); });
+    executor.corun_until([&](){ return t2.is_done(); });
+
+    return fu1.get() + fu2.get();
+  };
+
+  for (size_t i = 0; i <= 11; ++i) {
+    auto [tn, fun] = executor.dependent_async(std::bind(fib, i));
+    REQUIRE(fun.get() == fibonacci[i]);
+  }
+}
+
+TEST_CASE("DependentAsync.RecursiveFibonacci.1thread" * doctest::timeout(300)) {
+  recursive_fibonacci(1);
+}
+
+TEST_CASE("DependentAsync.RecursiveFibonacci.2threads" * doctest::timeout(300)) {
+  recursive_fibonacci(2);
+}
+
+TEST_CASE("DependentAsync.RecursiveFibonacci.4threads" * doctest::timeout(300)) {
+  recursive_fibonacci(4);
+}
+
+TEST_CASE("DependentAsync.RecursiveFibonacci.8threads" * doctest::timeout(300)) {
+  recursive_fibonacci(8);
+}
 
