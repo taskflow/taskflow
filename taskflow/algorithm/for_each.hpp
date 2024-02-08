35fd539b1937a52613c6bab7a4273edc883a4689
Samuel Ayala <samayala22@gmail.com>
2024-02-08 12:04:36 -0500

    use more casts
    idk this is messy

--- taskflow/algorithm/for_each.hpp
+++ taskflow/algorithm/for_each.hpp
@@ -84,16 +84,16 @@ using is_range_func = std::is_invocable_r<void, C, T, T>;
 // Function: make_for_each_index_task
 template <typename T, typename C, typename P = DefaultPartitioner>
 TF_FORCE_INLINE auto make_for_each_index_task(T b, T e, C c, P part = P()){
-  static_assert(std::is_integral<T>::value, "Begin and end values must be an integral type.");
+  using T_t = std::decay_t<unwrap_ref_decay_t<T>>;
+
+  static_assert(std::is_integral<T_t>::value, "Begin and end values must be an integral type.");
   static_assert(
-        std::disjunction<is_index_func<T, C>, is_range_func<T, C>>::value,
+        std::disjunction<is_index_func<T_t, C>, is_range_func<T_t, C>>::value,
         "C must be either a void function taking one int or two ints"
   );
   constexpr bool is_index_callable = is_index_func<T, C>::value;
 
   using namespace std::string_literals;
-
-  using T_t = std::decay_t<unwrap_ref_decay_t<T>>;
 
   return [=] (Runtime& rt) mutable {
 
@@ -107,17 +107,18 @@ TF_FORCE_INLINE auto make_for_each_index_task(T b, T e, C c, P part = P()){
     }
 
     size_t W = rt.executor().num_workers();
-    size_t N = end-beg;
+    T_t n = end-beg;
+    size_t N = static_cast<size_t>(n);
 
     // only myself - no need to spawn another graph
     if(W <= 1 || N <= part.chunk_size()) {
       TF_MAKE_LOOP_TASK(
         if constexpr(is_index_callable) {
-          for(T_t i=0; i<N; i++) {
+          for(T_t i=0; i<n; i++) {
             c(i);
           }
         } else {
-          c(0, N);
+          c(0, n);
         }
       );
       return;
@@ -138,10 +139,10 @@ TF_FORCE_INLINE auto make_for_each_index_task(T b, T e, C c, P part = P()){
               [&](size_t part_b, size_t part_e) {
                 if constexpr(is_index_callable) {
                   for(size_t i=part_b; i<part_e; i++) {
-                    c(i);
+                    c(static_cast<T_t>(i));
                   }
                 } else {
-                  c(part_b, part_e);
+                  c(static_cast<T_t>(part_b), static_cast<T_t>(part_e));
                 }
               }
             ); 
@@ -160,10 +161,10 @@ TF_FORCE_INLINE auto make_for_each_index_task(T b, T e, C c, P part = P()){
             [&](size_t part_b, size_t part_e) {
               if constexpr(is_index_callable) {
                 for(size_t i=part_b; i<part_e; i++) {
-                  c(i);
+                  c(static_cast<T_t>(i));
                 }
               } else {
-                c(part_b, part_e);
+                c(static_cast<T_t>(part_b), static_cast<T_t>(part_e));
               }
             }
           ); 
