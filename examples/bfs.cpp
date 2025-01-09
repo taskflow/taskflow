7985fd8caa97b2e8345b8d2711fb3d2de1213218
Tsung-Wei Huang <twhuang@vpn-pa-full-ptr-76.engr.wisc.edu>
2025-01-08 23:42:57 -0600

    backup bfs

--- examples/bfs.cpp
+++ examples/bfs.cpp
@@ -1,156 +1,157 @@
-#include <taskflow/taskflow.hpp>
+#include <cstdint>
+#include <limits>
 
-using eidType = uint64_t;
-using vidType = uint64_t;
-using weight_type = uint64_t;
-
-constexpr auto MAX_DISTANCE = std::numeric_limits<weight_type>::max();
+using eidType = std::uint64_t;
+using vidType = std::uint64_t;
+using weight_type = std::uint64_t;
 
 struct BaseGraph {};
 
 // ---- start here
 
-class Graph : public BaseGraph {
+#include <taskflow/taskflow.hpp>
+  
+struct Worker {
+  std::vector<vidType> local_frontiers;
 
+  std::thread thread;
+  std::condition_variable cv;
+  std::mutex mutex;
+  std::function<bool()> task;
+
+  void wait_for_task() {
+    std::function<bool()> local;
+    while(true) {
+      {
+        std::unique_lock lock(mutex);
+        cv.wait(lock, [&]{ return task != nullptr; });
+        local = std::move(task);
+      }
+      if(local() == false) {
+        break;
+      }
+    }
+  }
+
+  template <typename C>
+  auto async(C&& callable) {
+
+    std::promise<void> promise;
+    auto fu = promise.get_future();
+
+    {
+      std::scoped_lock lock(mutex);
+      task = [callable=std::forward<C>(callable), p=tf::MoC{std::move(promise)}]() mutable {
+        auto res = callable();
+        p.object.set_value(); 
+        return res;
+      };
+    }
+
+    cv.notify_one();
+
+    return fu;
+  }
+
+  //Worker() : thread ([this](){ wait_for_task(); }) { }
+};
+
+struct Helper {
+  Helper() : 
+    workers(std::thread::hardware_concurrency())
+  {
+    //std::cout << "helper created!!! " << this << std::endl;
+    ////spawn threads
+    for(size_t i=0; i<workers.size(); i++) {
+      workers[i].thread = std::thread([&w=workers[i]](){
+        w.wait_for_task();
+      });
+    }
+  }
+
+  ~Helper() {
+    // stop workers
+    for(size_t i=0; i<workers.size(); i++) {
+      workers[i].async([](){ return false; });
+    }
+    
+    // join threads
+    for(size_t i=0; i<workers.size(); i++) {
+      workers[i].thread.join();
+    }
+  }
+
+  std::vector<Worker> workers;
+};
+
+Helper& get_helper() {
+  static Helper helper;
+  return helper;
+};
+
+class Graph : public BaseGraph {
+  
   eidType* rowptr;
   vidType* col;
   uint64_t N;
   uint64_t M;
+  
+  constexpr static size_t alpha {65536};
+  //constexpr static size_t alpha {1};
+  constexpr static size_t gamma {64};  // minimum chunk size
+
+  size_t W;
+
+  std::unique_ptr<vidType[]> this_frontiers;
+  std::unique_ptr<vidType[]> next_frontiers;
 
   public:
+  
 
   Graph(eidType* in_rowptr, vidType* in_col, uint64_t in_N, uint64_t in_M) :
-    rowptr(in_rowptr), col(in_col), N(in_N), M(in_M) {
+    rowptr        (in_rowptr), 
+    col           (in_col), 
+    N             (in_N), 
+    M             (in_M),
+    W             ((N + M) < alpha ? 0 : get_helper().workers.size()),
+    this_frontiers(std::make_unique<vidType[]>(N)),
+    next_frontiers(std::make_unique<vidType[]>(N)) {
+
+    //tf::Executor executor(W);
   }
   
   ~Graph() {
+
     // destructor logic.
     // If you perform any memory allocations with malloc, new, etc. you must free
     //   them here to avoid memory leaks.
-  }
-
-
-  enum FrontierType : int {
-    THIS = 0,
-    NEXT = 1,
-    MAXN = 2
-  };
   
-  struct Worker {
-    std::array<std::vector<vidType>, FrontierType::MAXN> frontiers;
-    size_t prefix_sum[FrontierType::MAXN];
-  };
-  
-  // find min
-  size_t find_from(std::vector<Worker>& workers, size_t value, FrontierType type) {
-  
-    size_t beg = 0;
-    size_t end = workers.size() - 1;
-    size_t mid;
-    size_t best = end;
-  
-    do {
-      mid = (beg+end)/2;
-  
-      if(value < workers[mid].prefix_sum[type]) {
-        best = std::min(best, mid);
-        end = mid;
-      }
-      else {
-        beg = mid + 1;
-      }
-  
-    }while(beg < end);
-  
-    return best;
-  }
-  
-  // find min
-  size_t find_to(std::vector<Worker>& workers, size_t value, FrontierType type) {
-  
-    size_t beg = 0;
-    size_t end = workers.size() - 1;
-    size_t mid;
-    size_t best = end;
-  
-    do {
-      mid = (beg+end)/2;
-  
-      if(value <= workers[mid].prefix_sum[type]) {
-        best = std::min(best, mid);
-        end = mid;
-      }
-      else {
-        beg = mid + 1;
-      }
-  
-    }while(beg < end);
-  
-    return best;
-  }
-  
-  
-  
-  size_t num_frontiers(std::vector<Worker>& workers, FrontierType type) {
-    size_t n = 0;
-    for(auto& w : workers) {
-      n += w.frontiers[type].size();
-      w.prefix_sum[type] = n;
-    }
-    return n;
   }
   
   void BFS(vidType source, weight_type * distances) {
+
+    ((N + M) < alpha) ? BFS_sequential(source, distances) : BFS_parallel(source, distances);
+  }
+
   
-    const size_t W = std::thread::hardware_concurrency();
-    const size_t alpha = 1;
-  
-    // W must be greater than 0
-    if(W == 0) {
-      throw std::runtime_error("not enough worker to do the work");
-    }
+  void BFS_parallel(vidType source, weight_type * distances) {
+
+    //printf("parallel BFS\n");
     
-    tf::Executor executor(W);
-  
-    std::vector<Worker> workers(W);
-    
-    // TODO
+    std::atomic<size_t> num_next_frontiers(0);
+    this_frontiers[0] = source;
     distances[source] = 0;
-    workers[0].frontiers[FrontierType::THIS].push_back(source);
+    size_t num_this_frontiers = 1;
+
+    std::vector<std::future<void>> futures;
   
-    //std::srand(std::time(nullptr)); // use current time as seed for random generator
-    //const size_t alpha = std::rand() % 1 + 1;
-    //printf("alpha = %zu\n", alpha);
-    //for(size_t w=0; w<workers.size(); w++) {
-    //  workers[w].frontiers[FrontierType::THIS].resize(std::rand() % 2, 0);
-    //  printf("worker %2zu resize to %5zu frontiers\n", w, workers[w].frontiers[FrontierType::THIS].size());
-    //}
-  
-  
-  
-    
-    while(true) {
-  
-      // calculate the number of this frontiers
-      size_t num_this_frontiers = num_frontiers(workers, FrontierType::THIS);
+    while(num_this_frontiers) {
       
-      // no more tasks to do
-      if(num_this_frontiers == 0) {
-        break;
-      }
-      
-      // distribute this-frontiers among workers
-      size_t chunk_size = std::max(alpha, (num_this_frontiers + W - 1) / W);
-      size_t beg = 0;
-      size_t end;
-      size_t w = 0;
-      //std::atomic<size_t> test = 0;
+      // distribute the work among workers using static scheduling algorithm
+      size_t chunk_size = std::max(gamma, (num_this_frontiers + W - 1) / W);
+      for(size_t w=0, beg=0;;) {
   
-      //printf("chunk_size = %zu; num_this_frontiers = %zu\n", chunk_size, num_this_frontiers);
-  
-      while(beg < num_this_frontiers) {
-  
-        end = std::min(beg + chunk_size, num_this_frontiers);
+        size_t end = std::min(beg + chunk_size, num_this_frontiers);
   
         // has task
         auto task = [
@@ -158,52 +159,27 @@ class Graph : public BaseGraph {
           w,             // for next frontiers
           beg,
           end,
-          &workers,
-          distances
+          distances,
+          &num_next_frontiers
           //&test
         ] () mutable {
   
-          auto& next_frontier = workers[w].frontiers[FrontierType::NEXT];
-  
-          next_frontier.clear();
-  
-          // workers[from].frontiers - workers[to].frontiers
-          auto from = find_from(workers, beg, FrontierType::THIS);
-          auto to   = find_to(workers, end, FrontierType::THIS);
+          auto& local_frontiers = get_helper().workers[w].local_frontiers;
+          local_frontiers.clear();
           
-          //printf("worker %2zu handle discrete domain [%zu, %zu) spanning [%zu, %zu]\n", w, beg, end, from, to);
-  
-          for(size_t i=from; i<=to; i++) {
-            // fetch this-frontiers from worker i
-            auto& this_frontier = workers[i].frontiers[FrontierType::THIS];
-            auto cur_prefix_sum = workers[i].prefix_sum[FrontierType::THIS];
-            auto pre_prefix_sum = (i ? workers[i-1].prefix_sum[FrontierType::THIS] : 0);
-            auto f = std::max(beg, pre_prefix_sum) - (i ? pre_prefix_sum : 0);
-            auto t = std::min(end, cur_prefix_sum) - (i ? pre_prefix_sum : 0);
-            //printf("worker %2zu on frontiers %2zu: [%5zu, %5zu) of size %zu\n", w, i, f, t, t-f);
-  
-            //if(t > this_frontier.size() || f > this_frontier.size()) {
-            //  throw std::runtime_error("failed 1");
-            //} 
-            
-            // DEBUG
-            //test += (t-f);
-            //for(size_t j=f; j<t; j++) {
-            //  this_frontier[j]++;
-            //  test += this_frontier[j];
-            //}
-            
-            // one step of bfs
-            for(size_t j=f; j<t; j++) {
-              auto u = this_frontier[j];
-              for (eidType k = rowptr[u]; k < rowptr[u+1]; k++) {
-                auto v = col[k];
-                if(__sync_bool_compare_and_swap(&distances[v], MAX_DISTANCE, distances[u] + 1)) {
-                  next_frontier.push_back(v);
-                }
+          for(size_t i=beg; i<end; i++) {
+            auto u = this_frontiers[i];
+            for (eidType k = rowptr[u]; k < rowptr[u+1]; k++) {
+              auto v = col[k];
+              if(v != u && __sync_bool_compare_and_swap(&distances[v], std::numeric_limits<weight_type>::max(), distances[u] + 1)) {
+                local_frontiers.push_back(v);
               }
             }
           }
+          size_t next_beg = num_next_frontiers.fetch_add(local_frontiers.size(), std::memory_order_relaxed);
+          std::memcpy(next_frontiers.get() + next_beg, local_frontiers.data(), local_frontiers.size()*sizeof(vidType));
+
+          return true;
         };
   
         // last task
@@ -212,27 +188,97 @@ class Graph : public BaseGraph {
           break;
         }
         else {
-          executor.silent_async(task);
+          //executor.silent_async(task);
+          futures.push_back( get_helper().workers[w-1].async(task) );
         }
       }
+
+      //// distribute work using dynamic scheduler
+      //std::atomic<size_t> next(0);
+
+      //for(size_t w=0; w<W;) {
+      //
+      //  // has task
+      //  auto task = [
+      //    this,
+      //    w,             // for next frontiers
+      //    distances,
+      //    &next,
+      //    &num_next_frontiers,
+      //    num_this_frontiers
+      //    //&test
+      //  ] () mutable {
   
-      executor.wait_for_all();
-        
-      //if(test != num_this_frontiers) {
-      //  printf("test = %zu\n", test.load());
-      //  throw std::runtime_error("failed 2\n");
+      //    auto& local_frontiers = workers[w].local_frontiers;
+      //    local_frontiers.clear();
+      //    
+      //    size_t beg = next.fetch_add(gamma, std::memory_order_relaxed);
+
+      //    while(beg < num_this_frontiers) {
+      //      size_t end = std::min(beg + gamma, num_this_frontiers);
+      //      for(size_t i=beg; i<end; i++) {
+      //        auto u = this_frontiers[i];
+      //        for (eidType k = rowptr[u]; k < rowptr[u+1]; k++) {
+      //          auto v = col[k];
+      //          if(v != u && __sync_bool_compare_and_swap(&distances[v], std::numeric_limits<weight_type>::max(), distances[u] + 1)) {
+      //            local_frontiers.push_back(v);
+      //          }
+      //        }
+      //      }
+      //      beg = next.fetch_add(gamma, std::memory_order_relaxed);
+      //    }
+      //      
+      //    size_t next_beg = num_next_frontiers.fetch_add(local_frontiers.size(), std::memory_order_relaxed);
+      //    std::memcpy(next_frontiers.get() + next_beg, local_frontiers.data(), local_frontiers.size()*sizeof(vidType));
+
+      //    return true;
+      //  };
+      //
+      //  // last task
+      //  if(++w == W) {
+      //    task();
+      //    break;
+      //  }
+      //  else {
+      //    //executor.silent_async(task);
+      //    futures.push_back( workers[w-1].async(task) );
+      //  }
       //}
+
   
-      // TODO: swap this and next fontiers
-      for(size_t wid=0; wid < workers.size(); wid++) {
-        //for(size_t i=0; i<workers[wid].frontiers[FrontierType::THIS].size(); i++) {
-        //  if(workers[wid].frontiers[FrontierType::THIS][i] != 1) {
-        //    throw std::runtime_error("failed 3\n");
-        //  }
-        //}   
-        workers[wid].frontiers[FrontierType::THIS].clear();
-        std::swap(workers[wid].frontiers[FrontierType::THIS], workers[wid].frontiers[FrontierType::NEXT]); 
+      //executor.wait_for_all();
+      for(auto& fu : futures) {
+        fu.get();
       }
+      futures.clear();
+
+      // swap this and next frontiers
+      std::swap(this_frontiers, next_frontiers);
+      num_this_frontiers = num_next_frontiers.exchange(0, std::memory_order_relaxed); 
+        
+    }
+  }
+
+  void BFS_sequential(vidType source, weight_type * distances) {
+    
+    this_frontiers[0] = source;
+    distances[source] = 0;
+    size_t num_this_frontiers = 1;
+
+    while (num_this_frontiers) {
+      size_t num_next_frontiers = 0;
+      for(size_t t = 0; t<num_this_frontiers; t++) {
+        auto u = this_frontiers[t];
+        for(auto i = rowptr[u]; i < rowptr[u+1]; i++) {
+          auto v = col[i];
+          if (v != u && distances[u] + 1 < distances[v]) {
+            distances[v] = distances[u] + 1;
+            next_frontiers[num_next_frontiers++] = v;
+          }
+        }
+      }
+      std::swap(this_frontiers, next_frontiers);
+      num_this_frontiers = num_next_frontiers;
     }
   }
 
@@ -244,17 +290,24 @@ int main() {
   const size_t M = 12;
   std::array<eidType, N+1> row = {0, 2, 5, 7, 10, 12};
   std::array<vidType, M> col = {1, 4, 0, 2, 3, 1, 3, 1, 2, 4, 0, 3};
-  std::vector<weight_type> distances(N, MAX_DISTANCE);
+  std::vector<weight_type> distances_p(N, std::numeric_limits<weight_type>::max());
+  std::vector<weight_type> distances_s(N, std::numeric_limits<weight_type>::max());
   
   Graph graph(row.data(), col.data(), N, M);
-  graph.BFS(0, distances.data());
+
+  //graph.BFS_parallel(0, distances_p.data());
+  graph.BFS(0, distances_p.data());
+  graph.BFS_sequential(0, distances_s.data());
 
   //BFS(0, distances.data(), row.data(), col.data(), N);
 
-  for(size_t i=0; i<distances.size(); i++) {
-    printf("distances[%5zu] = ", i);
-    std::cout << distances[i] << '\n';
+  for(size_t i=0; i<N; i++) {
+    printf("distances[%5zu] = (%5llu, %5llu)\n", i, distances_p[i], distances_s[i]);
+    if(distances_p[i] != distances_s[i]) {
+      throw std::runtime_error("distance mismatch");
+    }
   }
+  
 
 }
 
