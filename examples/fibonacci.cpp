0007dd38512b704b6c7e456f0b9499139bbb36ef
Tsung-Wei Huang <twhuang@w198-3-v4.eduroam.dynamic.rbg.tum.de>
2025-08-03 13:42:33 +0200

    updated

--- examples/fibonacci.cpp
+++ examples/fibonacci.cpp
@@ -7,44 +7,6 @@ tf::Executor& get_executor() {
   return executor;
 }
 
-// ------------------------------------------------------------------------------------------------
-// implementation using subflow (slower)
-// ------------------------------------------------------------------------------------------------
-
-size_t spawn_subflow(size_t n, tf::Subflow& sbf) {
-  
-  if (n < 2) {
-    return n;
-  }
-
-  size_t res1, res2;
-
-  sbf.emplace([&res1, n] (tf::Subflow& sbf_n_1) { res1 = spawn_subflow(n - 1, sbf_n_1); } );
-  sbf.emplace([&res2, n] (tf::Subflow& sbf_n_2) { res2 = spawn_subflow(n - 2, sbf_n_2); } );
-
-  sbf.join();
-  return res1 + res2;
-}
-
-size_t fibonacci_subflow(size_t N) {
-
-  size_t res;  // result
-
-  tf::Taskflow taskflow("fibonacci");
-
-  taskflow.emplace([&res, N] (tf::Subflow& sbf) {
-    res = spawn_subflow(N, sbf);
-  }).name(std::to_string(N));
-
-  get_executor().run(taskflow).wait();
-  
-  return res;
-}
-
-// ------------------------------------------------------------------------------------------------
-// implementation using async (faster)
-// ------------------------------------------------------------------------------------------------
-
 size_t spawn_async(size_t N, tf::Runtime& rt) {
 
   if (N < 2) {
@@ -72,24 +34,15 @@ size_t fibonacci_async(size_t N) {
 
 int main(int argc, char* argv[]) {
 
-  if(argc != 3) {
-    std::cerr << "usage: ./fibonacci N [subflow|async]\n";
+  if(argc != 2) {
+    std::cerr << "usage: ./fibonacci N\n";
     std::exit(EXIT_FAILURE);
   }
 
   size_t N = std::atoi(argv[1]);
 
   auto tbeg = std::chrono::steady_clock::now();
-  if(std::strcmp(argv[2], "subflow") == 0) {
-    printf("fib[%zu] (with subflow) = %zu\n", N, fibonacci_subflow(N));
-  }
-  else if(std::strcmp(argv[2], "async") == 0) {
-    printf("fib[%zu] (with async) = %zu\n", N, fibonacci_async(N));
-  }
-  else {
-    std::cerr << "unrecognized method " << argv[2] << '\n';
-    std::exit(EXIT_FAILURE);
-  }
+  printf("fib[%zu] = %zu\n", N, fibonacci_async(N));
   auto tend = std::chrono::steady_clock::now();
 
   std::cout << "elapsed time: " 
