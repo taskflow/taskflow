96aba5eb1ccb6e5927d68d0933409eade122fea5
Hatem feckry <hatemfeckry347@gmail.com>
2026-04-14 01:23:07 +0200

    added partitioner tests

--- unittests/test_fill.cpp
+++ unittests/test_fill.cpp
@@ -114,35 +114,35 @@ template <typename T> void fill_n_sync(size_t W, size_t N, size_t C, T val) {
   REQUIRE(a == std_a);
 }
 
-TEST_CASE("ParallelFill.int.1.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillN.int.1.10000" * doctest::timeout(300)) {
   fill_n_sync<int>(1, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelFill.int.2.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillN.int.2.10000" * doctest::timeout(300)) {
   fill_n_sync<int>(2, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelFill.int.3.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillN.int.3.10000" * doctest::timeout(300)) {
   fill_n_sync<int>(3, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelFill.int.4.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillN.int.4.10000" * doctest::timeout(300)) {
   fill_n_sync<int>(4, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelFill.ldouble.1.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillN.ldouble.1.10000" * doctest::timeout(300)) {
   fill_n_sync<long double>(1, 10000, 5000, 12.);
 }
 
-TEST_CASE("ParallelFill.ldouble.2.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillN.ldouble.2.10000" * doctest::timeout(300)) {
   fill_n_sync<long double>(2, 10000, 5000, 12.);
 }
 
-TEST_CASE("ParallelFill.ldouble.3.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillN.ldouble.3.10000" * doctest::timeout(300)) {
   fill_n_sync<long double>(3, 10000, 5000, 12.);
 }
 
-TEST_CASE("ParallelFill.ldouble.4.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillN.ldouble.4.10000" * doctest::timeout(300)) {
   fill_n_sync<long double>(4, 10000, 5000, 12.);
 }
 
@@ -162,35 +162,35 @@ template <typename T> void fill_n_async(size_t W, size_t N, size_t C, T val) {
   REQUIRE(a == std_a);
 }
 
-TEST_CASE("ParallelFillAsync.int.1.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillNAsync.int.1.10000" * doctest::timeout(300)) {
   fill_n_async<int>(1, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelFillAsync.int.2.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillNAsync.int.2.10000" * doctest::timeout(300)) {
   fill_n_async<int>(2, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelFillAsync.int.3.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillNAsync.int.3.10000" * doctest::timeout(300)) {
   fill_n_async<int>(3, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelFillAsync.int.4.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillNAsync.int.4.10000" * doctest::timeout(300)) {
   fill_n_async<int>(4, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelFillAsync.ldouble.1.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillNAsync.ldouble.1.10000" * doctest::timeout(300)) {
   fill_n_async<long double>(1, 10000, 5000, 12.);
 }
 
-TEST_CASE("ParallelFillAsync.ldouble.2.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillNAsync.ldouble.2.10000" * doctest::timeout(300)) {
   fill_n_async<long double>(2, 10000, 5000, 12.);
 }
 
-TEST_CASE("ParallelFillAsync.ldouble.3.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillNAsync.ldouble.3.10000" * doctest::timeout(300)) {
   fill_n_async<long double>(3, 10000, 5000, 12.);
 }
 
-TEST_CASE("ParallelFillAsync.ldouble.4.10000" * doctest::timeout(300)) {
+TEST_CASE("ParallelFillNAsync.ldouble.4.10000" * doctest::timeout(300)) {
   fill_n_async<long double>(4, 10000, 5000, 12.);
 }
 
@@ -225,9 +225,7 @@ TEST_CASE("ParallelFillStatefulMixed.4" * doctest::timeout(300)) {
   tf::Taskflow taskflow;
 
   std::vector<int>::iterator beg;
-  auto init = taskflow.emplace([&]() {
-    beg = a.begin();
-  });
+  auto init = taskflow.emplace([&]() { beg = a.begin(); });
   auto fill_task = taskflow.fill(std::ref(beg), a.end(), 12);
   init.precede(fill_task);
 
@@ -259,4 +257,188 @@ TEST_CASE("ParallelFillNStateful.4" * doctest::timeout(300)) {
   std::fill_n(std_a.begin(), 5000, 12);
 
   REQUIRE(a == std_a);
+}
+
+template <typename P> void fill_partioners(size_t W, size_t N, P part) {
+  std::vector<int> a(N);
+
+  tf::Executor executor(W);
+  tf::Taskflow taskflow;
+
+  taskflow.fill(a.begin(), a.end(), 12, part);
+  executor.run(taskflow).wait();
+
+  std::vector<int> std_a(N);
+  std::fill(std_a.begin(), std_a.end(), 12);
+
+  REQUIRE(a == std_a);
+
+  std::vector<int> b(N);
+
+  executor.async(tf::make_fill_task(b.begin(), b.end(), 12, part));
+
+  executor.wait_for_all();
+  std::vector<int> std_b(N);
+  std::fill(std_b.begin(), std_b.end(), 12);
+
+  REQUIRE(b == std_b);
+}
+
+TEST_CASE("ParallelFill.Static.int.1.10000" * doctest::timeout(300)) {
+  fill_partioners(1, 10000, tf::StaticPartitioner<>());
+}
+
+TEST_CASE("ParallelFill.Static.int.2.10000" * doctest::timeout(300)) {
+  fill_partioners(2, 10000, tf::StaticPartitioner<>());
+}
+
+TEST_CASE("ParallelFill.Static.int.3.10000" * doctest::timeout(300)) {
+  fill_partioners(3, 10000, tf::StaticPartitioner<>());
+}
+
+TEST_CASE("ParallelFill.Static.int.4.10000" * doctest::timeout(300)) {
+  fill_partioners(4, 10000, tf::StaticPartitioner<>());
+}
+
+
+TEST_CASE("ParallelFill.Dynamic.int.1.10000" * doctest::timeout(300)) {
+  fill_partioners(1, 10000, tf::DynamicPartitioner<>());
+}
+
+TEST_CASE("ParallelFill.Dynamic.int.2.10000" * doctest::timeout(300)) {
+  fill_partioners(2, 10000, tf::DynamicPartitioner<>());
+}
+
+TEST_CASE("ParallelFill.Dynamic.int.3.10000" * doctest::timeout(300)) {
+  fill_partioners(3, 10000, tf::DynamicPartitioner<>());
+}
+
+TEST_CASE("ParallelFill.Dynamic.int.4.10000" * doctest::timeout(300)) {
+  fill_partioners(4, 10000, tf::DynamicPartitioner<>());
+}
+
+
+TEST_CASE("ParallelFill.Random.int.1.10000" * doctest::timeout(300)) {
+  fill_partioners(1, 10000, tf::RandomPartitioner<>());
+}
+
+TEST_CASE("ParallelFill.Random.int.2.10000" * doctest::timeout(300)) {
+  fill_partioners(2, 10000, tf::RandomPartitioner<>());
+}
+
+TEST_CASE("ParallelFill.Random.int.3.10000" * doctest::timeout(300)) {
+  fill_partioners(3, 10000, tf::RandomPartitioner<>());
+}
+
+TEST_CASE("ParallelFill.Random.int.4.10000" * doctest::timeout(300)) {
+  fill_partioners(4, 10000, tf::RandomPartitioner<>());
+}
+
+
+TEST_CASE("ParallelFill.Default.int.1.10000" * doctest::timeout(300)) {
+  fill_partioners(1, 10000, tf::DefaultPartitioner());
+}
+
+TEST_CASE("ParallelFill.Default.int.2.10000" * doctest::timeout(300)) {
+  fill_partioners(2, 10000, tf::DefaultPartitioner());
+}
+
+TEST_CASE("ParallelFill.Default.int.3.10000" * doctest::timeout(300)) {
+  fill_partioners(3, 10000, tf::DefaultPartitioner());
+}
+
+TEST_CASE("ParallelFill.Default.int.4.10000" * doctest::timeout(300)) {
+  fill_partioners(4, 10000, tf::DefaultPartitioner());
+}
+
+template <typename P> void fill_n_partioners(size_t W, size_t N, P part) {
+  std::vector<int> a(N);
+
+  tf::Executor executor(W);
+  tf::Taskflow taskflow;
+
+  taskflow.fill_n(a.begin(), N / 2, 12, part);
+  executor.run(taskflow).wait();
+
+  std::vector<int> std_a(N);
+  std::fill_n(std_a.begin(), N / 2, 12);
+
+  REQUIRE(a == std_a);
+
+  std::vector<int> b(N, 0);
+
+  executor.async(tf::make_fill_n_task(b.begin(), N / 2, 12, part));
+
+  executor.wait_for_all();
+  std::vector<int> std_b(N, 0);
+  std::fill_n(std_b.begin(), N / 2, 12);
+
+  REQUIRE(b == std_b);
+}
+
+TEST_CASE("ParallelFillN.Static.int.1.10000" * doctest::timeout(300)) {
+  fill_n_partioners(1, 10000, tf::StaticPartitioner<>());
+}
+
+TEST_CASE("ParallelFillN.Static.int.2.10000" * doctest::timeout(300)) {
+  fill_n_partioners(2, 10000, tf::StaticPartitioner<>());
+}
+
+TEST_CASE("ParallelFillN.Static.int.3.10000" * doctest::timeout(300)) {
+  fill_n_partioners(3, 10000, tf::StaticPartitioner<>());
+}
+
+TEST_CASE("ParallelFillN.Static.int.4.10000" * doctest::timeout(300)) {
+  fill_n_partioners(4, 10000, tf::StaticPartitioner<>());
+}
+
+
+TEST_CASE("ParallelFillN.Dynamic.int.1.10000" * doctest::timeout(300)) {
+  fill_n_partioners(1, 10000, tf::DynamicPartitioner<>());
+}
+
+TEST_CASE("ParallelFillN.Dynamic.int.2.10000" * doctest::timeout(300)) {
+  fill_n_partioners(2, 10000, tf::DynamicPartitioner<>());
+}
+
+TEST_CASE("ParallelFillN.Dynamic.int.3.10000" * doctest::timeout(300)) {
+  fill_n_partioners(3, 10000, tf::DynamicPartitioner<>());
+}
+
+TEST_CASE("ParallelFillN.Dynamic.int.4.10000" * doctest::timeout(300)) {
+  fill_n_partioners(4, 10000, tf::DynamicPartitioner<>());
+}
+
+
+TEST_CASE("ParallelFillN.Random.int.1.10000" * doctest::timeout(300)) {
+  fill_n_partioners(1, 10000, tf::RandomPartitioner<>());
+}
+
+TEST_CASE("ParallelFillN.Random.int.2.10000" * doctest::timeout(300)) {
+  fill_n_partioners(2, 10000, tf::RandomPartitioner<>());
+}
+
+TEST_CASE("ParallelFillN.Random.int.3.10000" * doctest::timeout(300)) {
+  fill_n_partioners(3, 10000, tf::RandomPartitioner<>());
+}
+
+TEST_CASE("ParallelFillN.Random.int.4.10000" * doctest::timeout(300)) {
+  fill_n_partioners(4, 10000, tf::RandomPartitioner<>());
+}
+
+
+TEST_CASE("ParallelFillN.Default.int.1.10000" * doctest::timeout(300)) {
+  fill_n_partioners(1, 10000, tf::DefaultPartitioner());
+}
+
+TEST_CASE("ParallelFillN.Default.int.2.10000" * doctest::timeout(300)) {
+  fill_n_partioners(2, 10000, tf::DefaultPartitioner());
+}
+
+TEST_CASE("ParallelFillN.Default.int.3.10000" * doctest::timeout(300)) {
+  fill_n_partioners(3, 10000, tf::DefaultPartitioner());
+}
+
+TEST_CASE("ParallelFillN.Default.int.4.10000" * doctest::timeout(300)) {
+  fill_n_partioners(4, 10000, tf::DefaultPartitioner());
 }
--- unittests/test_generate.cpp
+++ unittests/test_generate.cpp
@@ -123,35 +123,35 @@ template <typename T> void generate_n_sync(size_t W, size_t N, size_t C, T val) 
   REQUIRE(a == std_a);
 }
 
-TEST_CASE("ParallelGenerate.int.1.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateN.int.1.10000" * doctest::timeout(300)){
   generate_n_sync<int>(1, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelGenerate.int.2.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateN.int.2.10000" * doctest::timeout(300)){
   generate_n_sync<int>(2, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelGenerate.int.3.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateN.int.3.10000" * doctest::timeout(300)){
   generate_n_sync<int>(3, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelGenerate.int.4.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateN.int.4.10000" * doctest::timeout(300)){
   generate_n_sync<int>(4, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelGenerate.ldouble.1.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateN.ldouble.1.10000" * doctest::timeout(300)){
   generate_n_sync<long double>(1, 10000, 5000, 12.);
 }
 
-TEST_CASE("ParallelGenerate.ldouble.2.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateN.ldouble.2.10000" * doctest::timeout(300)){
   generate_n_sync<long double>(2, 10000, 5000, 12.);
 }
 
-TEST_CASE("ParallelGenerate.ldouble.3.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateN.ldouble.3.10000" * doctest::timeout(300)){
   generate_n_sync<long double>(3, 10000, 5000, 12.);
 }
 
-TEST_CASE("ParallelGenerate.ldouble.4.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateN.ldouble.4.10000" * doctest::timeout(300)){
   generate_n_sync<long double>(4, 10000, 5000, 12.);
 }
 
@@ -174,35 +174,35 @@ template <typename T> void generate_n_async(size_t W, size_t N, size_t C, T val)
   REQUIRE(a == std_a);
 }
 
-TEST_CASE("ParallelGenerateAsync.int.1.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateNAsync.int.1.10000" * doctest::timeout(300)){
   generate_n_async<int>(1, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelGenerateAsync.int.2.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateNAsync.int.2.10000" * doctest::timeout(300)){
   generate_n_async<int>(2, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelGenerateAsync.int.3.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateNAsync.int.3.10000" * doctest::timeout(300)){
   generate_n_async<int>(3, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelGenerateAsync.int.4.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateNAsync.int.4.10000" * doctest::timeout(300)){
   generate_n_async<int>(4, 10000, 5000, 12);
 }
 
-TEST_CASE("ParallelGenerateAsync.ldouble.1.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateNAsync.ldouble.1.10000" * doctest::timeout(300)){
   generate_n_async<long double>(1, 10000, 5000, 12.);
 }
 
-TEST_CASE("ParallelGenerateAsync.ldouble.2.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateNAsync.ldouble.2.10000" * doctest::timeout(300)){
   generate_n_async<long double>(2, 10000, 5000, 12.);
 }
 
-TEST_CASE("ParallelGenerateAsync.ldouble.3.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateNAsync.ldouble.3.10000" * doctest::timeout(300)){
   generate_n_async<long double>(3, 10000, 5000, 12.);
 }
 
-TEST_CASE("ParallelGenerateAsync.ldouble.4.10000" * doctest::timeout(300)){
+TEST_CASE("ParallelGenerateNAsync.ldouble.4.10000" * doctest::timeout(300)){
   generate_n_async<long double>(4, 10000, 5000, 12.);
 }
 
@@ -280,4 +280,194 @@ TEST_CASE("ParallelGenerateNStateful.1" * doctest::timeout(300)) {
   std::generate_n(std_a.begin(), 5000, gen);
 
   REQUIRE(a == std_a);
+}
+
+template <typename P> void generate_partioners(size_t W, size_t N, P part) {
+   auto gen = [](){
+    return 12;
+  };
+  std::vector<int> a(N);
+
+  tf::Executor executor(W);
+  tf::Taskflow taskflow;
+
+  taskflow.generate(a.begin(), a.end(), gen, part);
+  executor.run(taskflow).wait();
+
+  std::vector<int> std_a(N);
+  std::generate(std_a.begin(), std_a.end(), gen);
+
+  REQUIRE(a == std_a);
+
+  std::vector<int> b(N);
+
+  executor.async(tf::make_generate_task(b.begin(), b.end(), gen, part));
+
+  executor.wait_for_all();
+  std::vector<int> std_b(N);
+  std::generate(std_b.begin(), std_b.end(), gen);
+
+  REQUIRE(b == std_b);
+}
+
+TEST_CASE("ParallelGenerate.Static.int.1.10000" * doctest::timeout(300)) {
+  generate_partioners(1, 10000, tf::StaticPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerate.Static.int.2.10000" * doctest::timeout(300)) {
+  generate_partioners(2, 10000, tf::StaticPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerate.Static.int.3.10000" * doctest::timeout(300)) {
+  generate_partioners(3, 10000, tf::StaticPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerate.Static.int.4.10000" * doctest::timeout(300)) {
+  generate_partioners(4, 10000, tf::StaticPartitioner<>());
+}
+
+
+TEST_CASE("ParallelGenerate.Dynamic.int.1.10000" * doctest::timeout(300)) {
+  generate_partioners(1, 10000, tf::DynamicPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerate.Dynamic.int.2.10000" * doctest::timeout(300)) {
+  generate_partioners(2, 10000, tf::DynamicPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerate.Dynamic.int.3.10000" * doctest::timeout(300)) {
+  generate_partioners(3, 10000, tf::DynamicPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerate.Dynamic.int.4.10000" * doctest::timeout(300)) {
+  generate_partioners(4, 10000, tf::DynamicPartitioner<>());
+}
+
+
+TEST_CASE("ParallelGenerate.Random.int.1.10000" * doctest::timeout(300)) {
+  generate_partioners(1, 10000, tf::RandomPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerate.Random.int.2.10000" * doctest::timeout(300)) {
+  generate_partioners(2, 10000, tf::RandomPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerate.Random.int.3.10000" * doctest::timeout(300)) {
+  generate_partioners(3, 10000, tf::RandomPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerate.Random.int.4.10000" * doctest::timeout(300)) {
+  generate_partioners(4, 10000, tf::RandomPartitioner<>());
+}
+
+
+TEST_CASE("ParallelGenerate.Default.int.1.10000" * doctest::timeout(300)) {
+  generate_partioners(1, 10000, tf::DefaultPartitioner());
+}
+
+TEST_CASE("ParallelGenerate.Default.int.2.10000" * doctest::timeout(300)) {
+  generate_partioners(2, 10000, tf::DefaultPartitioner());
+}
+
+TEST_CASE("ParallelGenerate.Default.int.3.10000" * doctest::timeout(300)) {
+  generate_partioners(3, 10000, tf::DefaultPartitioner());
+}
+
+TEST_CASE("ParallelGenerate.Default.int.4.10000" * doctest::timeout(300)) {
+  generate_partioners(4, 10000, tf::DefaultPartitioner());
+}
+
+template <typename P> void generate_n_partioners(size_t W, size_t N, P part) {
+   auto gen = [](){
+    return 12;
+  };
+  std::vector<int> a(N);
+
+  tf::Executor executor(W);
+  tf::Taskflow taskflow;
+
+  taskflow.generate_n(a.begin(), N / 2, gen, part);
+  executor.run(taskflow).wait();
+
+  std::vector<int> std_a(N);
+  std::generate_n(std_a.begin(), N / 2, gen);
+
+  REQUIRE(a == std_a);
+
+  std::vector<int> b(N, 0);
+
+  executor.async(tf::make_generate_n_task(b.begin(), N / 2, gen, part));
+
+  executor.wait_for_all();
+  std::vector<int> std_b(N, 0);
+  std::generate_n(std_b.begin(), N / 2, gen);
+
+  REQUIRE(b == std_b);
+}
+
+TEST_CASE("ParallelGenerateN.Static.int.1.10000" * doctest::timeout(300)) {
+  generate_n_partioners(1, 10000, tf::StaticPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerateN.Static.int.2.10000" * doctest::timeout(300)) {
+  generate_n_partioners(2, 10000, tf::StaticPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerateN.Static.int.3.10000" * doctest::timeout(300)) {
+  generate_n_partioners(3, 10000, tf::StaticPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerateN.Static.int.4.10000" * doctest::timeout(300)) {
+  generate_n_partioners(4, 10000, tf::StaticPartitioner<>());
+}
+
+
+TEST_CASE("ParallelGenerateN.Dynamic.int.1.10000" * doctest::timeout(300)) {
+  generate_n_partioners(1, 10000, tf::DynamicPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerateN.Dynamic.int.2.10000" * doctest::timeout(300)) {
+  generate_n_partioners(2, 10000, tf::DynamicPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerateN.Dynamic.int.3.10000" * doctest::timeout(300)) {
+  generate_n_partioners(3, 10000, tf::DynamicPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerateN.Dynamic.int.4.10000" * doctest::timeout(300)) {
+  generate_n_partioners(4, 10000, tf::DynamicPartitioner<>());
+}
+
+
+TEST_CASE("ParallelGenerateN.Random.int.1.10000" * doctest::timeout(300)) {
+  generate_n_partioners(1, 10000, tf::RandomPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerateN.Random.int.2.10000" * doctest::timeout(300)) {
+  generate_n_partioners(2, 10000, tf::RandomPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerateN.Random.int.3.10000" * doctest::timeout(300)) {
+  generate_n_partioners(3, 10000, tf::RandomPartitioner<>());
+}
+
+TEST_CASE("ParallelGenerateN.Random.int.4.10000" * doctest::timeout(300)) {
+  generate_n_partioners(4, 10000, tf::RandomPartitioner<>());
+}
+
+
+TEST_CASE("ParallelGenerateN.Default.int.1.10000" * doctest::timeout(300)) {
+  generate_n_partioners(1, 10000, tf::DefaultPartitioner());
+}
+
+TEST_CASE("ParallelGenerateN.Default.int.2.10000" * doctest::timeout(300)) {
+  generate_n_partioners(2, 10000, tf::DefaultPartitioner());
+}
+
+TEST_CASE("ParallelGenerateN.Default.int.3.10000" * doctest::timeout(300)) {
+  generate_n_partioners(3, 10000, tf::DefaultPartitioner());
+}
+
+TEST_CASE("ParallelGenerateN.Default.int.4.10000" * doctest::timeout(300)) {
+  generate_n_partioners(4, 10000, tf::DefaultPartitioner());
 }
